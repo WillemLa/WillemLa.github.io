86d440caafb01cce1efc8af708c6b93687d77e63
wilapage <Willem.Lapage@UGent.be>
2025-11-20 10:56:21 +0100

    Update concepts exec student 2

--- students/data/2/concepts/exercise_2.js
+++ students/data/2/concepts/exercise_2.js
@@ -3,49 +3,35 @@ window.exerciseData = {
   context:
     "Laat om de beurt de leds van rechts naar links knipperen. Zodra de meest linkse LED knippert herbegin je helemaal rechts",
 
-  code: `#include <Wire.h>
-
-#include <Dwenguino.h>
-
-#include <LiquidCrystal.h>
-
-void schrijfNaarLCD(int tekst) {
-  dwenguinoLCD.clear();
-  dwenguinoLCD.setCursor(0,0);
-  dwenguinoLCD.print(tekst);
-}
-
-int led;
-
-void init() {
-  led = 1;
-  schrijfNaarLCD(String(led));
-  LEDS = led;
-  delay(300);
-}
-
-void maalTwee() {
-  led = led * 2;
-  schrijfNaarLCD(String(led));
-  LEDS = led;
-  delay(300);
-}
+  code: `int Doel;
+int HuidigGetal;
+int LedGetal;
 
 void setup()
 {
   initDwenguino();
-
-  init();
+  Doel = (random(0, 255));
+  HuidigGetal = 0;
+  LedGetal = 1;
+  dwenguinoLCD.setCursor(0,0);
+  dwenguinoLCD.print(String(Doel));
+  LEDS = LedGetal;
 }
-
 
 void loop()
 {
-    for ( int j = 0 ; j <= 6 ; j+=1) {
-      maalTwee();
+    if (digitalRead(SW_W) == PRESSED) {
+      if (LedGetal < 64) {
+        LedGetal = LedGetal * 2;
+        LEDS = LedGetal;
+      }
     }
-    init();
-
+    if (digitalRead(SW_E) == PRESSED) {
+      if (LedGetal > 2) {
+        LedGetal = LedGetal / 2;
+        LEDS = LedGetal;
+      }
+    }
 }`,
   advice: [
     "Bespreek met Ben hoe de LED-sequenties zijn opgebouwd en welke controle-structuren gebruikt worden.",
