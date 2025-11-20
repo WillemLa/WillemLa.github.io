e53c2689cab502609912512e5a8f0472ad362c05
wilapage <Willem.Lapage@UGent.be>
2025-11-20 11:23:59 +0100

    Change concept execs student 4

--- students/data/4/concepts/exercise_1.js
+++ students/data/4/concepts/exercise_1.js
@@ -3,54 +3,80 @@ window.exerciseData = {
   context:
     "Laat eerst de LEDs op positie 0, 2, 4, 6 en vervolgens de LEDs op positie 1, 3, 5, 7 branden. Zorg dat dit proces zich blijft herhalen.",
 
-  code: `#include <Wire.h>
+  code: `DCMotor dcMotor1(MOTOR_1_0, MOTOR_1_1);
+DCMotor dcMotor2(MOTOR_2_0, MOTOR_2_1);
 
-#include <Dwenguino.h>
+void Links() {
+  dcMotor1.setSpeed(100);
+  delay(250);
+  Stop();
+}
 
-#include <LiquidCrystal.h>
+DCMotor dcMotor2(MOTOR_2_0, MOTOR_2_1);
 
-int item;
+void Rechts() {
+  dcMotor2.setSpeed(100);
+  delay(250);
+  Stop();
+}
 
-void schrijfNaarScherm(int tekst) {
-  dwenguinoLCD.clear();
-  dwenguinoLCD.setCursor(0,0);
-  dwenguinoLCD.print(tekst);
+void Vooruit() {
+  dcMotor1.setSpeed(100);
+  dcMotor2.setSpeed(100);
+  delay(250);
+  Stop();
+}
+
+void Achteruit() {
+  dcMotor1.setSpeed((-100));
+  dcMotor2.setSpeed((-100));
+  delay(250);
+  Stop();
+}
+
+void Stop() {
+  dcMotor1.setSpeed(0);
+  dcMotor2.setSpeed(0);
+}
+
+void Knipper() {
+  for ( int i = 0 ; i < 3 ; i+=1) {
+  LEDS = 0b11111111;
+  delay(250);
+  LEDS = 0b00000000;
+  delay(250);
+}
+
+void Knipper() {
+  for ( int i = 0 ; i < 3 ; i+=1) {
+  LEDS = 0b11111111;
+  delay(250);
+  LEDS = 0b00000000;
+  delay(250);
 }
 
 void setup()
 {
   initDwenguino();
-
-  item = 1;
-  LEDS = item;
-  schrijfNaarScherm(String(item));
-  delay(300);
 }
-
 
 void loop()
 {
-    for ( int i = 0 ; i <= 2 ; i+=1) {
-      item = item * 4;
-      LEDS = item;
-      schrijfNaarScherm(String(item));
-      delay(300);
+    if (digitalRead(SW_C) == PRESSED) {
+      Knipper();
     }
-    item = 2;
-    LEDS = item;
-    schrijfNaarScherm(String(item));
-    delay(300);
-    for ( int i = 0 ; i <= 2 ; i+=1) {
-      item = item * 4;
-      LEDS = item;
-      schrijfNaarScherm(String(item));
-      delay(300);
+    if (digitalRead(SW_N) == PRESSED) {
+      Vooruit();
     }
-    item = 1;
-    LEDS = item;
-    schrijfNaarScherm(String(item));
-    delay(300);
-
+    if (digitalRead(SW_E) == PRESSED) {
+      Rechts();
+    }
+    if (digitalRead(SW_S) == PRESSED) {
+      Achteruit();
+    }
+    if (digitalRead(SW_W) == PRESSED) {
+      Links();
+    }
 }`,
   advice: [
     "Bespreek met Daniel waar functies en lussen worden toegepast in Afwisseling.",
--- students/data/4/concepts/exercise_2.js
+++ students/data/4/concepts/exercise_2.js
@@ -2,75 +2,66 @@ window.exerciseData = {
   student: "Daniel Wu",
   context:
     "Laat om de beurt de leds van rechts naar links knipperen. Zodra de meest linkse LED knippert herbegin je helemaal rechts",
-  code: `#include <Wire.h>
-
-#include <Dwenguino.h>
-
-#include <LiquidCrystal.h>
-
-int led;
-
-void schrijfNaarLCD(int tekst) {
-  dwenguinoLCD.clear();
-  dwenguinoLCD.setCursor(0,0);
-  dwenguinoLCD.print(tekst);
-}
+  code: `int Doel;
+int HuidigGetal;
+int LedGetal;
 
 void setup()
 {
   initDwenguino();
-
-  led = 1;
+  Doel = (random(0, 255));
+  HuidigGetal = 0;
+  LedGetal = 1;
+  dwenguinoLCD.setCursor(0,0);
+  dwenguinoLCD.print(String("Doel: ") + String(Doel));
+  LEDS = LedGetal;
 }
 
-// <tag:Loop>
 void loop()
 {
-// </tag:Loop>
-    // <tag:Loop>
-    if(led = 1)
-    {
-    // </tag:Loop>
-// <tag:Function>
-        schrijfNaarLCD(String(led));
-        LEDS = led;
-        delay(300);
-        led = led * 2;
-        schrijfNaarLCD(String(led));
-        LEDS = led;
-        delay(300);
-        led = led * 2;
-        schrijfNaarLCD(String(led));
-        LEDS = led;
-        delay(300);
-        led = led * 2;
-        schrijfNaarLCD(String(led));
-        LEDS = led;
-        delay(300);
-        led = led * 2;
-        schrijfNaarLCD(String(led));
-        LEDS = led;
-        delay(300);
-        led = led * 2;
-        schrijfNaarLCD(String(led));
-        LEDS = led;
-        delay(300);
-        led = led * 2;
-        schrijfNaarLCD(String(led));
-        LEDS = led;
-        delay(300);
-        led = led * 2;
-        schrijfNaarLCD(String(led));
-        LEDS = led;
-        delay(300);
-        led = 1;
-    // </tag:Function>
-    // <tag:Loop>
+    if (digitalRead(SW_W) == PRESSED) {
+      if (LedGetal <= 128) {
+        LedGetal = LedGetal * 2;
+        LEDS = LedGetal;
+      }
     }
-// </tag:Loop>
-    // <tag:Loop>
-}
-        // </tag:Loop>`,
+    if (digitalRead(SW_E) == PRESSED) {
+      if (LedGetal >= 2) {
+        LedGetal = LedGetal / 2;
+        LEDS = LedGetal;
+      }
+    }
+    if (digitalRead(SW_C) == PRESSED) {
+      HuidigGetal = HuidigGetal + LedGetal;
+      if (Doel == HuidigGetal) {
+        dwenguinoLCD.clear();
+        dwenguinoLCD.setCursor(0,1);
+        dwenguinoLCD.print(String("Proficiat!"));
+        LEDS = 0b11111111;
+        delay(250);
+        LEDS = 0b00000000;
+        delay(250);
+        LEDS = 0b11111111;
+        delay(250);
+        LEDS = 0b00000000;
+        delay(250);
+        LEDS = 0b11111111;
+        delay(250);
+        LEDS = 0b00000000;
+        delay(250);
+        dwenguinoLCD.clear();
+        Doel = (random(0, 255));
+        HuidigGetal = 0;
+        LedGetal = 1;
+        dwenguinoLCD.setCursor(0,0);
+        dwenguinoLCD.print(String("Doel: ") + String(Doel));
+        LEDS = LedGetal;
+      } else {
+        dwenguinoLCD.setCursor(0,1);
+        dwenguinoLCD.print(String("Huidig: ") + String(HuidigGetal));
+      }
+    }
+}`,
   advice: [
     "Bespreek met Daniel het belang van herhaling vermijden en manier om dit te doen. Leg ook uit dat in een 'if-statement', een enkele '=' een toewijzing is en '==' een vergelijking.",
   ],
